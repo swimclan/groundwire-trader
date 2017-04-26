c872b5cc742f516a3f058dbb2c2bdb28b361bd64
Matthew Herron <matt@groundwire.co>
2017-04-25 22:10:05 -0500

    cleaned up socket connection logic and event logic

--- controllers/tradePositions.js
+++ controllers/tradePositions.js
@@ -3,20 +3,51 @@
 var utils = require('../utils');
 var _ = require('lodash');
 var Stream = require('../streams/stream');
+var Positions = require('../collections/positions');
 
 module.exports = function(req, res, next) {
     var exclusions = [];
     if (_.has(req.body, 'exclusions')) {
-        exclusions = utils.splitStringList(req.body.exclusions);
+        exclusions = exclusions.concat(utils.splitStringList(req.body.exclusions));
     }
 
-    let connect_handler = () => {
-        console.log('Connected to the GroundWire price socket!');
-        res.json({message: "Connected to the price stream"});
-    };
+    new Positions().fetch()
+    .then((positions) => {
+        var priceStream = openStream('GEVO')
+        .then((priceStream) => {
+            res.json({message: "Connected to GroundWire socket"})
+            priceStream.on('frame', (frame) => {
+                return null;
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    });
+}
 
-    let priceStream = new Stream({ticker: 'WFT', connect_handler: connect_handler});
-    priceStream.frame.on('change', function(frame) {
-        console.log(frame);
+var openStream = function(ticker) {
+    return new Promise((resolve, reject) => {
+        let connect_handler = () => {
+            console.log('Connected to the GroundWire price socket!');
+        };
+
+        let close_handler = (reason) => {
+            console.log('Disconnected from the GroundWire price socket...  Goodbye!');
+            console.log(reason);
+        };
+
+        let error_handler = (err) => {
+            console.log('There was an error connecting to the GroundWire price socket');
+            reject(err);
+        };
+
+        let outStream = new Stream({
+            ticker: ticker,
+            connect_handler: connect_handler,
+            close_handler: close_handler,
+            error_handler: error_handler
+        });
+        resolve(outStream);
     });
 }
